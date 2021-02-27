e2552ba7001d1a393de6d934fd0f15b2d735467c
Alyssa Ross <hi@alyssa.is>
2021-02-27 03:16:04 +0000

    microsoft/surface: stdenv.lib -> lib
    stdenv.lib is deprecated.

--- microsoft/surface/kernel/linux-5.10.2/linux-5.10.2.nix
+++ microsoft/surface/kernel/linux-5.10.2/linux-5.10.2.nix
@@ -1,6 +1,9 @@
-{ stdenv, buildPackages, fetchurl, perl, buildLinux, modDirVersionArg ? null, ... } @ args:
+{ lib, stdenv, buildPackages, fetchurl, perl, buildLinux
+, modDirVersionArg ? null
+, ...
+} @ args:
 
-with stdenv.lib;
+with lib;
 
 buildLinux (args // rec {
   version = "5.10.2";
